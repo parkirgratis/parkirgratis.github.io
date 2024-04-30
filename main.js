c21a753f54c67b427f2f8ea9dfe39f687b0fb3a7
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-04-30 07:48:11 +0700

    perbarui

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 import Map from 'https://cdn.skypack.dev/ol/Map.js';
 import View from 'https://cdn.skypack.dev/ol/View.js';
 import TileLayer from 'https://cdn.skypack.dev/ol/layer/Tile.js';
-import XYZ from 'https://cdn.skypack.dev/ol/source/XYZ.js';
+import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js';
+import { useGeographic } from 'https://cdn.skypack.dev/ol/proj.js'; 
 
-new Map({
-    target: 'map',
-    layers: [
-        new TileLayer({
-            source: new XYZ({
-                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-            })
-        })
-    ],
-    view: new View({
-        center: [106.8272, -6.2088],
-        zoom: 5 // memperbesar zoom
+useGeographic();
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
     })
+  ],
+  view: new View({
+    center: [107.6098, -6.9175], //Koordinat Kota Bandung
+    zoom: 9
+  })
 });
