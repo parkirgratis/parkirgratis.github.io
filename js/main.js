278d24bd1928cc35455e72b39377de8ad7e0408c
Revan232 <revanyuda001@gmail.com>
2024-05-13 23:15:00 +0700

    update

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ const map = new Map({
 });
 
 const markerCoords = [
-    [107.57806170827054, -6.87616450819235],
+    [107.57806170827054, -6.87616450819235], //alfamart 
     [107.57699001587744, -6.878240461265228],
     [107.57443704813844, -6.865543210439596],
     [107.58023768006228, -6.8739512071239846],
@@ -35,18 +35,21 @@ const popupsData = [
     {
         coordinate: markerCoords[0],
         content: `
-            <div style="background-color: white; padding: 10px;">
-                <table>
-                    <tr>
-                        <th>Alfamart sarimanah</th>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <th>Jl. Sarimanah Kel No.87, RT.04/RW.09,<br>
-                         Sarijadi, Kec. Sukasari, Kota Bandung, 
-                         Jawa Barat 40151</th>
-                        <td></td>
-                    </tr>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg" id="iniTabel">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <tr>
+        <th scope="col" class="px-6 py-3">
+            #ID
+        </th>
+        <th scope="col" class="px-6 py-3">
+            #Nama
+        </th>
+        <th scope="col" class="px-6 py-3">
+            #Lokasi
+        </th>
+        <th scope="col" class="px-6 py-3">
+            #Fasilitas
+        </th>
                 </table>
             </div>`
     },
--- js/temp/table.js
+++ js/temp/table.js
@@ -1,38 +1,21 @@
 export let isiTabel = `
   <tbody>
-            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    #Id
-                </th>
-                <td class="px-6 py-4">
-                    #Nama
-                </td>
-                <td class="px-6 py-4">
-                    #Posisi
-                </td>
-                <td class="px-6 py-4">
-                    #Tinggi
-                </td>
-                <td class="px-6 py-4">
-                    #Berat
-                </td>
-                <td class="px-6 py-4">
-                    #TanggalLahir
-                </td>
-                <td class="px-6 py-4">
-                    #Negara
-                </td>
-                <td class="px-6 py-4">
-                    #NoPunggung
-                </td>
-                <td class="px-6 py-4">
-                #Statistik
-                </td>       
-                <td class="px-6 py-4">
-                    #Penampilan
-                </td><td class="px-6 py-4">
-                #Gol
-            </td>
-            </tr>
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg" id="iniTabel">
+  <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+  <tr>
+  <th scope="col" class="px-6 py-3">
+      #ID
+  </th>
+  <th scope="col" class="px-6 py-3">
+      #Nama
+  </th>
+  <th scope="col" class="px-6 py-3">
+      #Lokasi
+  </th>
+  <th scope="col" class="px-6 py-3">
+      #Fasilitas
+  </th>
+          </table>
+      </div>
         </tbody>
 `;
