2c4688c9891a39036427a4f11e9b1a7eac575307
Revan232 <revanyuda001@gmail.com>
2024-05-29 23:21:39 +0700

    Frontend ||| mengubah pengambilan data popup

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,6 @@ import Map from 'https://cdn.skypack.dev/ol/Map.js';
 import View from 'https://cdn.skypack.dev/ol/View.js';
 import TileLayer from 'https://cdn.skypack.dev/ol/layer/Tile.js';
 import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js';
-
 import { fromLonLat } from 'https://cdn.skypack.dev/ol/proj.js';
 import { createMarker } from './controller/markers.js';
 import { createPopups, displayPopup } from './controller/popups.js';
@@ -36,170 +35,67 @@ fetch('https://parkirgratis.github.io/data/marker.json')
     .catch(error => console.error('Error fetching marker data:', error));
 
 
-//ambil data dari https://parkirgratis.github.io/data/lokasi.json masukin ke popusdata (looping)
-const popupsData = [
-    {
-        coordinate: [107.57806170827054, -6.87616450819235],
-        content: `
-            <div style="background-color: white; padding: 10px;">
-                <table>
-                    <tr>
-                        <th>Alfamart sarimanah</th>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <th>Jl. Sarimanah Kel No.87, RT.04/RW.09,<br>
-                         Sarijadi, Kec. Sukasari, Kota Bandung, 
-                         Jawa Barat 40151</th>
-                        <td></td>
-                    </tr>
-                </table>
-            </div>`
-    },
-    {
-      coordinate: [107.57699001587744, -6.878240461265228],
-      content: `
-          <div style="background-color: white; padding: 10px;">
-              <table>
-                  <tr>
-                      <th>Alfamart sarimanah</th>
-                      <td></td>
-                  </tr>
-                  <tr>
-                      <th>Jl. Sarimanah Kel No.87, RT.04/RW.09,<br>
-                       Sarijadi, Kec. Sukasari, Kota Bandung, 
-                       Jawa Barat 40151</th>
-                      <td></td>
-                  </tr>
-              </table>
-          </div>`
-  },
-  {
-    coordinate:[107.57443704813844, -6.865543210439596],
-    content: `
-        <div style="background-color: white; padding: 10px;">
-            <table>
-                <tr>
-                    <th>Alfamart sarimanah</th>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th>Jl. Sarimanah Kel No.87, RT.04/RW.09,<br>
-                     Sarijadi, Kec. Sukasari, Kota Bandung, 
-                     Jawa Barat 40151</th>
-                    <td></td>
-                </tr>
-            </table>
-        </div>`
-},
-{
-  coordinate:[107.58023768006228, -6.8739512071239846],
-  content: `
-      <div style="background-color: white; padding: 10px;">
-          <table>
-              <tr>
-                  <th>Alfamart sarimanah</th>
-                  <td></td>
-              </tr>
-              <tr>
-                  <th>Jl. Sarimanah Kel No.87, RT.04/RW.09,<br>
-                   Sarijadi, Kec. Sukasari, Kota Bandung, 
-                   Jawa Barat 40151</th>
-                  <td></td>
-              </tr>
-          </table>
-      </div>`
-},
-{
-  coordinate: [107.57674995307603, -6.879421693090783],
-  content: `
-      <div style="background-color: white; padding: 10px;">
-          <table>
-              <tr>
-                  <th>Alfamart sarimanah</th>
-                  <td></td>
-              </tr>
-              <tr>
-                  <th>Jl. Sarimanah Kel No.87, RT.04/RW.09,<br>
-                   Sarijadi, Kec. Sukasari, Kota Bandung, 
-                   Jawa Barat 40151</th>
-                  <td></td>
-              </tr>
-          </table>
-      </div>`
-},
-{
-  coordinate: [107.55090379994242, -6.862020504462997],
-  content: `
-      <div style="background-color: white; padding: 10px;">
-          <table>
-              <tr>
-                  <th>Alfamart sarimanah</th>
-                  <td></td>
-              </tr>
-              <tr>
-                  <th>Jl. Sarimanah Kel No.87, RT.04/RW.09,<br>
-                   Sarijadi, Kec. Sukasari, Kota Bandung, 
-                   Jawa Barat 40151</th>
-                  <td></td>
-              </tr>
-          </table>
-      </div>`
-},
-{
-  coordinate: [107.57613168830173, -6.8620573920747585],
-  content: `
-      <div style="background-color: white; padding: 10px;">
-          <table>
-              <tr>
-                  <th>Alfamart sarimanah</th>
-                  <td></td>
-              </tr>
-              <tr>
-                  <th>Jl. Sarimanah Kel No.87, RT.04/RW.09,<br>
-                   Sarijadi, Kec. Sukasari, Kota Bandung, 
-                   Jawa Barat 40151</th>
-                  <td></td>
-              </tr>
-          </table>
-      </div>`
-},
-{
-  coordinate: [107.69155583045713, -6.900889681458682],
-  content: `
-      <div style="background-color: white; padding: 10px;">
-          <table>
-              <tr>
-                  <th>Alfamart sarimanah</th>
-                  <td></td>
-              </tr>
-              <tr>
-                  <th>Jl. Sarimanah Kel No.87, RT.04/RW.09,<br>
-                   Sarijadi, Kec. Sukasari, Kota Bandung, 
-                   Jawa Barat 40151</th>
-                  <td></td>
-              </tr>
-          </table>
-      </div>`
-},
+let popupsData = [];
+fetch('https://parkirgratis.github.io/data/lokasi.json')
+    .then(response => response.json())
+    .then(data => {
+        if (Array.isArray(data)) {
+            data.forEach(item => {
+                if (item.lon && item.lat && item.nama_tempat && item.lokasi && item.fasilitas) {
+                    popupsData.push({
+                        coordinate: [item.lon, item.lat],
+                        content: `
+                            <div style="background-color: white; padding: 10px;">
+                                <table>
+                                    <tr>
+                                        <th>${item.nama_tempat}</th>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <th>${item.lokasi}</th>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <th>${item.fasilitas}</th>
+                                        <td></td>
+                                    </tr>
+                                </table>
+                            </div>`
+                    });
+                } else {
+                    console.error('Invalid popup data item:', item);
+                }
+            });
+        } else {
+            console.error('Popup data is not an array:', data);
+        }
+        console.log('Popup Data:', popupsData);
 
-  
-    // Add other popup data here
-];
+        initializeMapPopups(popupsData);
+    })
+    .catch(error => console.error('Error fetching popup data:', error));
 
+    let popups = [];
 
-const popups = createPopups(map, popupsData);
+    function initializeMapPopups(popupsData) {
+        popups = createPopups(map, popupsData);
+    }
 
-function createMapMarkers(markerCoords) {
-    const markers = markerCoords.map(coord => createMarker(map, coord));
-    markers.forEach((marker, index) => {
-        marker.getElement().addEventListener('click', () => {
-            const popup = popups[index];
-            const popupData = popupsData[index];
-            displayPopup(popup, popupData.coordinate, popupData.content);
+    function createMapMarkers(markerCoords) {
+        const markers = markerCoords.map(coord => createMarker(map, coord));
+        markers.forEach((marker, index) => {
+            marker.getElement().addEventListener('click', () => {
+                const popup = popups[index];
+                const popupData = popupsData[index];
+                if (popup && popupData && popupData.coordinate) {
+                    displayPopup(popup, popupData.coordinate, popupData.content);
+                } else {
+                    console.error('Popup or popup data not found for marker index:', index);
+                }
+            });
         });
-    });
-}
+    }
+    
 
 map.on('click', function(event) {
     popups.forEach(popup => {
