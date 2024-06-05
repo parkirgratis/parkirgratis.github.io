be6f9b4505d67831308ba69b91eb0b9e227a49f4
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-06-05 23:35:43 +0700

    Frontend ||| memperbaiki css pada image

--- javascript/main.js
+++ javascript/main.js
@@ -18,11 +18,12 @@ const map = new Map({
         zoom: 12
     })
 });
-
+// Mengambil data koordinat marker dari URL yang diberikan
 let markerCoords = [];
 fetch('https://parkirgratis.github.io/data/marker.json')
     .then(response => response.json())
     .then(data => {
+        // Memeriksa apakah data markers adalah array
         if (!Array.isArray(data.markers)) {
             console.error('Data marker bukan array:', data);
             return;
@@ -32,60 +33,67 @@ fetch('https://parkirgratis.github.io/data/marker.json')
     })
     .catch(error => console.error('Gagal mengambil data marker:', error));
 
+// Mengambil data untuk pop-up dari URL yang diberikan
+let popupsData = [];
+fetch('https://parkirgratis.github.io/data/lokasi.json')
+    .then(response => response.json())
+    .then(data => {
+        // Memeriksa apakah data adalah array
+        if (!Array.isArray(data)) {
+            console.error('Popup data bukan array:', data);
+            return;
+        }
+        // Memfilter dan memetakan data pop-up
+        popupsData = data.filter(item => item.lon && item.lat && item.nama_tempat && item.lokasi && item.fasilitas && item.gambar)
+                         .map(item => ({
+                             coordinate: [item.lon, item.lat],
+                             content: `
+                                <div class="popup-content">
+                                    <img src="${item.gambar}" alt="Gambar Tempat" style="width:100%; height:auto;">
+                                    <table>
+                                        <tr><th>Nama Tempat</th><td>${item.nama_tempat}</td></tr>
+                                        <tr><th>Lokasi</th><td>${item.lokasi}</td></tr>
+                                        <tr><th>Fasilitas</th><td>${item.fasilitas}</td></tr>
+                                    </table>
+                                </div>`
+                         }));
+        console.log('Popup Data:', popupsData);
+        initializeMapPopups(popupsData); // Menginisialisasi pop-up pada peta
+    })
+    .catch(error => console.error('Error fetching popup data:', error));
 
-    let popupsData = [];
-    fetch('https://parkirgratis.github.io/data/lokasi.json')
-        .then(response => response.json())
-        .then(data => {
-            if (!Array.isArray(data)) {
-                console.error('Popup data is not an array:', data);
-                return;
-            }
-            popupsData = data.filter(item => item.lon && item.lat && item.nama_tempat && item.lokasi && item.fasilitas && item.gambar)
-                             .map(item => ({
-                                 coordinate: [item.lon, item.lat],
-                                 content: `
-                                    <div class="popup-content">
-                                        <img src="${item.gambar}" alt="Gambar Tempat" style="width:100%; height:auto;">
-                                        <table>
-                                            <tr><th>Nama Tempat</th><td>${item.nama_tempat}</td></tr>
-                                            <tr><th>Lokasi</th><td>${item.lokasi}</td></tr>
-                                            <tr><th>Fasilitas</th><td>${item.fasilitas}</td></tr>
-                                        </table>
-                                    </div>`
-                             }));
-            console.log('Popup Data:', popupsData);
-            initializeMapPopups(popupsData);
-        })
-        .catch(error => console.error('Error fetching popup data:', error));
+// Array untuk menyimpan pop-up
 let popups = [];
 
+// Fungsi untuk menginisialisasi pop-up pada peta
 function initializeMapPopups(popupsData) {
     popups = createPopups(map, popupsData.map(item => ({
-        coordinate: [item.lon, item.lat],
-        content: `
-            <div class="popup-content">
-              
-                <table>
-                    <tr><th>Nama Tempat</th><td>${item.nama_tempat}</td></tr>
-                    <tr><th>Lokasi</th><td>${item.lokasi}</td></tr>
-                    <tr><th>Fasilitas</th><td>${item.fasilitas}</td></tr>
-                </table>
-            </div>`
+        coordinate: item.coordinate,
+        content: item.content
     })));
 }
 
 function createMapMarkers(markerCoords) {
+    // Membuat penanda (marker) untuk setiap koordinat yang diberikan
     const markers = markerCoords.map(coord => createMarker(map, coord));
+
+    // Menambahkan event listener ke setiap penanda
     markers.forEach((marker, index) => {
         marker.getElement().addEventListener('click', () => {
+            // Mengambil pop-up dan data pop-up berdasarkan indeks penanda
             const popup = popups[index];
             const popupData = popupsData[index];
+
+            // Jika pop-up dan data pop-up ditemukan
             if (popup && popupData && popupData.coordinate) {
+                // Menampilkan pop-up dengan konten yang sesuai pada koordinat yang diberikan
                 displayPopup(popup, popupData.coordinate, popupData.content);
+
+                // Menganimasikan tampilan peta untuk berfokus pada lokasi pop-up dengan zoom tingkat 14
                 map.getView().animate({ center: fromLonLat(popupData.coordinate), zoom: 14 });
             } else {
-                console.error('Popup or popup data not found for marker index:', index);
+                // Menampilkan pesan kesalahan jika pop-up atau data pop-up tidak ditemukan
+                console.error('Popup atau data pop-up tidak ditemukan untuk penanda dengan indeks:', index);
             }
         });
     });
@@ -96,34 +104,40 @@ map.on('click', function(event) {
         popup.setPosition(null);
     });
 });
-
 document.getElementById('showTableButton').addEventListener('click', function() {
+    // Mendapatkan elemen tabel berdasarkan ID
     const table = document.getElementById('placesTable');
+
+    // Periksa apakah tabel sudah ditampilkan
     if (table.style.display === 'block') {
         table.style.display = 'none'; // Sembunyikan tabel jika sudah ditampilkan
     } else {
         table.style.display = 'block'; // Tampilkan tabel jika sedang disembunyikan
+
+        // Mengambil data dari URL yang diberikan
         fetch('https://parkirgratis.github.io/data/lokasi.json')
-            .then(response => response.json())
+            .then(response => response.json()) // Mengonversi respons ke format JSON
             .then(data => {
                 const tbody = table.getElementsByTagName('tbody')[0];
                 tbody.innerHTML = ''; // Bersihkan isi tabel sebelumnya
 
+                // Menambahkan baris baru untuk setiap item dalam data
                 data.forEach(item => {
                     const row = document.createElement('tr');
                     const cell = document.createElement('td');
-                    cell.textContent = item.nama_tempat;
+                    cell.textContent = item.nama_tempat; // Menambahkan nama tempat ke sel tabel
                     row.appendChild(cell);
                     tbody.appendChild(row);
                 });
             })
             .catch(error => {
+                // Menampilkan pesan kesalahan jika terjadi masalah saat mengambil data
                 console.error('Error fetching data:', error);
             });
     }
 });
 
-// Tambahkan event listener ke peta untuk menyembunyikan tabel
+// Tambahkan event listener ke peta untuk menyembunyikan tabel saat peta diklik
 map.on('click', function() {
     document.getElementById('placesTable').style.display = 'none';
 });
