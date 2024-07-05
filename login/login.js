312d4f4aaaca8d47d5e4f241db00ebe06581f29b
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-07-05 19:05:43 +0700

    tambah show password

--- login/login.js
+++ login/login.js
@@ -21,3 +21,11 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
         document.getElementById('message').textContent = result.message;
     }
 });
+
+document.getElementById('togglePassword').addEventListener('click', function() {
+    const passwordField = document.getElementById('password');
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
+});
