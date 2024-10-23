819c9e13cc92ce4481b7fec0a425d402701b3ec9
hrisz <harissaefuloh@gmail.com>
2024-10-24 00:25:12 +0700

    fix: locate unreaded icon

--- pages/aboutus/aboutus.js
+++ pages/aboutus/aboutus.js
@@ -1,31 +1,28 @@
-$(document).ready(function() {
-    var autoplaySlider = $('#autoplay').lightSlider({
-        auto:true,
-        loop:true,
-        pauseOnHover: true,
-        onBeforeSlide: function (el) {
-            $('#current').text(el.getCurrentSlideCount());
-        } 
-    });
-    $('#total').text(autoplaySlider.getTotalSlideCount());
+$(document).ready(function () {
+  var autoplaySlider = $("#autoplay").lightSlider({
+    auto: true,
+    loop: true,
+    pauseOnHover: true,
+    onBeforeSlide: function (el) {
+      $("#current").text(el.getCurrentSlideCount());
+    },
+  });
+  $("#total").text(autoplaySlider.getTotalSlideCount());
 });
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
+};
