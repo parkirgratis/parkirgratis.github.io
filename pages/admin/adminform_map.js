74e269670f13d6251303033cddc17b6f9b0e3d27
hrisz <harissaefuloh@gmail.com>
2024-10-25 03:06:20 +0700

    add: submit form in dashboard

---
+++ pages/admin/adminform_map.js
@@ -0,0 +1,18 @@
+import Map from 'https://cdn.skypack.dev/ol/Map.js';
+import View from 'https://cdn.skypack.dev/ol/View.js';
+import TileLayer from 'https://cdn.skypack.dev/ol/layer/Tile.js';
+import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js';
+import { fromLonLat } from 'https://cdn.skypack.dev/ol/proj.js';
+
+const map = new Map({
+    target: 'map',
+    layers: [
+        new TileLayer({
+            source: new OSM()
+        })
+    ],
+    view: new View({
+        center: fromLonLat([107.6098, -6.9175]),
+        zoom: 12
+    })
+});
