c2c0f0f07cf0d353367afaeddd8e8dd516a4e1b0
hrisz <harissaefuloh@gmail.com>
2024-10-24 12:02:08 +0700

    mod: modify dashboard design to the new one

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -1,147 +1,50 @@
 document.addEventListener('DOMContentLoaded', async () => {
-            // Memuat data lokasi jika login berhasil
-            const dataDisplayTable = document.getElementById('dataDisplayTable').getElementsByTagName('tbody')[0];
+    const dataDisplayTable = document.getElementById('dataDisplayTable').getElementsByTagName('tbody')[0];
 
-            fetch('https://asia-southeast2-backend-438507.cloudfunctions.net/parkirgratisbackend/data/lokasi')
-                .then(response => response.json())
-                .then(data => {
-                    data.forEach(item => {
-                        const row = dataDisplayTable.insertRow();
-                        row.insertCell(0).textContent = item.nama_tempat;
-                        row.insertCell(1).textContent = item.lokasi;
-                        row.insertCell(2).textContent = item.fasilitas;
-                        row.insertCell(3).textContent = `${item.lon}, ${item.lat}`;
+    fetch('https://asia-southeast2-backend-438507.cloudfunctions.net/parkirgratisbackend/data/lokasi')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(item => {
+                const row = dataDisplayTable.insertRow();
+                
+                // Name Column
+                 let cell1 = row.insertCell(0);
+                 cell1.className = "px-6 py-4 whitespace-no-wrap border-b border-gray-200";
+                 cell1.innerHTML = `<div class="flex items-center">
+                                        <div class="ml-4">
+                                            <div class="text-sm font-medium leading-5 text-gray-900">${item.nama_tempat}</div>
+                                            <div class="md:block hidden text-sm leading-5 text-gray-500">${item.lokasi}</div>
+                                        </div>
+                                    </div>`;
+                
+                // Title (Lokasi) Column
+                let cell2 = row.insertCell(1);
+                cell2.className = "px-6 py-4 whitespace-no-wrap border-b border-gray-200";
+                cell2.innerHTML = `<div class="text-sm leading-5 text-gray-900">${item.lon}, ${item.lat}</div>`;
 
-                        const gambarUrl = item.gambar ? item.gambar : 'path/to/default/image.jpg';
-                        row.insertCell(4).innerHTML = `<img src="${gambarUrl}" alt="Gambar" style="width:100px;">`;
+                // Status (Fasilitas) Column
+                let cell3 = row.insertCell(2);
+                cell3.className = "px-6 py-4 whitespace-no-wrap border-b border-gray-200";
+                cell3.innerHTML = `
+                    <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
+                        ${item.fasilitas}
+                    </span>`;
 
-                        const actionsCell = row.insertCell(5);
-                        actionsCell.innerHTML = `
-                            <button type="button" style="background-color: #2ecc71;" onclick="showUpdateForm('${item._id}', '${item.nama_tempat}', '${item.lokasi}', '${item.fasilitas}', ${item.lon}, ${item.lat}, '${gambarUrl}')">Update</button>
-                            <button type="button" style="background-color: #e74c3c;" onclick="deleteData('${item._id}', ${item.lon}, ${item.lat}, this)">Delete</button>
-                        `;
-                    });
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
+                let cell4 = row.insertCell(3);
+                cell4.className = "px-6 py-4 whitespace-no-wrap border-b border-gray-200";
+                cell4.innerHTML = `<img class="w-20 h-20" src="${item.gambar ? item.gambar : 'path/to/default/image.jpg'}" alt="Gambar">`;
 
-            const modal = document.getElementById('updateModal');
-            const span = document.getElementsByClassName('close')[0];
-            span.onclick = function () {
-                modal.style.display = 'none';
-            }
-            window.onclick = function (event) {
-                if (event.target == modal) {
-                    modal.style.display = 'none';
-                }
-            }
-
-            document.getElementById('updateForm').addEventListener('submit', function (event) {
-                event.preventDefault();
-
-                const id = document.getElementById('updateId').value;
-                const updatedData = {
-                    _id: id,
-                    nama_tempat: document.getElementById('namaTempat').value,
-                    lokasi: document.getElementById('lokasi').value,
-                    fasilitas: document.getElementById('fasilitas').value,
-                    lon: parseFloat(document.getElementById('lon').value),
-                    lat: parseFloat(document.getElementById('lat').value),
-                    gambar: document.getElementById('gambar').value
-                };
-
-                fetch('https://asia-southeast2-backend-438507.cloudfunctions.net/parkirgratisbackend/data/tempat', {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(updatedData)
-                })
-                .then(response => response.json().then(data => ({ status: response.status, body: data })))
-                .then(({ status, body }) => {
-                    if (status === 200) {
-                        alert('Data updated successfully');
-                        modal.style.display = 'none';
-                        location.reload(); 
-                    } else {
-                        alert(`Error updating data: ${body.message}`);
-                    }
-                })
-                .catch(error => {
-                    console.error('Error updating data:', error);
-                });
+                // Actions (Update/Delete Buttons) Column
+                let cell5 = row.insertCell(4);
+                cell5.className = "px-6 py-4 whitespace-no-wrap border-b border-gray-200";
+                cell5.innerHTML = `
+                                    <div class="flex space-x-2">
+                                        <button type="button" class="text-white bg-green-500 px-2 py-1 rounded-md" onclick="showUpdateForm('${item._id}', '${item.nama_tempat}', '${item.lokasi}', '${item.fasilitas}', ${item.lon}, ${item.lat}, '${item.gambar}')">Update</button>
+                                        <button type="button" class="text-white bg-red-500 px-2 py-1 rounded-md" onclick="deleteData('${item._id}', ${item.lon}, ${item.lat}, this)">Delete</button>
+                                    </div>`;
             });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 });
-
-window.showUpdateForm = function(id, namaTempat, lokasi, fasilitas, lon, lat, gambar) {
-    const modal = document.getElementById('updateModal');
-    modal.style.display = 'block';
-
-    document.getElementById('updateId').value = id;
-    document.getElementById('namaTempat').value = namaTempat;
-    document.getElementById('lokasi').value = lokasi;
-    document.getElementById('fasilitas').value = fasilitas;
-    document.getElementById('lon').value = lon;
-    document.getElementById('lat').value = lat;
-    document.getElementById('gambar').value = gambar;
-
-    const gambarPreview = document.getElementById('gambarPreview');
-    gambarPreview.src = gambar;
-    gambarPreview.style.display = 'block';
-}
-
-window.deleteData = function(id, lon, lat, button) {
-    const confirmation = confirm('Are you sure you want to delete this data?');
-    if (!confirmation) {
-        return;
-    }
-
-    console.log(`Deleting item with ID: ${id}`);
-
-    const coordinates = [
-        [lon, lat]
-    ];
-
-    fetch('https://asia-southeast2-backend-438507.cloudfunctions.net/parkirgratisbackend/data/tempat', {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ id: id })
-    })
-    .then(response => response.json().then(data => ({ status: response.status, body: data })))
-    .then(({ status, body }) => {
-        if (status === 200) {
-            const coordData = {
-                _id: id,
-                markers: coordinates
-            };
-
-            fetch('https://asia-southeast2-backend-438507.cloudfunctions.net/parkirgratisbackend/data/koordinat', {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(coordData)
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Coordinates deleted successfully:', data);
-                alert('Coordinates deleted successfully!');
-                location.reload(); 
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('Failed to delete coordinates!');
-            });
-        } else {
-            console.error('Failed to delete the main data:', body);
-            alert('Failed to delete the main data!');
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('An error occurred while deleting the main data!');
-    });
-};
