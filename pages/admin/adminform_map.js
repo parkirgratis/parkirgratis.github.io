0e0e08b465759ef0445d25d57dc061b2aec5cbdc
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-10-29 21:44:33 +0700

    Add : click event to map for displaying longitude and latitude in input data

--- pages/admin/adminform_map.js
+++ pages/admin/adminform_map.js
@@ -2,8 +2,10 @@ import Map from 'https://cdn.skypack.dev/ol/Map.js';
 import View from 'https://cdn.skypack.dev/ol/View.js';
 import TileLayer from 'https://cdn.skypack.dev/ol/layer/Tile.js';
 import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js';
-import { fromLonLat } from 'https://cdn.skypack.dev/ol/proj.js';
+import { fromLonLat, toLonLat } from 'https://cdn.skypack.dev/ol/proj.js';
+import { fromEvent } from 'https://cdn.skypack.dev/rxjs';
 
+// Initialize the map
 const map = new Map({
     target: 'map',
     layers: [
@@ -15,4 +17,16 @@ const map = new Map({
         center: fromLonLat([107.6098, -6.9175]),
         zoom: 12
     })
+});
+
+// tambah saat klik map
+map.on('click', (event) => {
+    // mendapatkan lon lat saat mengklik map
+    const coordinates = toLonLat(event.coordinate);
+    const longitude = coordinates[0];
+    const latitude = coordinates[1];
+
+    
+    document.getElementById('long').value = longitude.toFixed(6); 
+    document.getElementById('lat').value = latitude.toFixed(6); 
 });
