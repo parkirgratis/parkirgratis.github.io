c6c00a7a63b825e18fa2adae586e27531a4b28fe
hrisz <harissaefuloh@gmail.com>
2024-10-30 20:42:08 +0700

    mod: change width for saran column to make it more readable in mobile screen

--- pages/admin/saran.js
+++ pages/admin/saran.js
@@ -11,17 +11,17 @@ document.addEventListener('DOMContentLoaded', async () => {
 
                 // Gmail kolom
                 let cell1 = row.insertCell(0);
-                cell1.className = "px-10 py-4 whitespace-nowrap border-b border-gray-200";
+                cell1.className = "px-10 py-4 border-b border-gray-200";
                 cell1.innerHTML = `<div class="text-sm text-gray-900">${item.gmail || 'No Gmail provided'}</div>`;
 
                 // Nama kolom
                 let cell2 = row.insertCell(1);
-                cell2.className = "px-6 py-4 whitespace-nowrap border-b border-gray-200";
+                cell2.className = "px-6 py-4 border-b border-gray-200";
                 cell2.innerHTML = `<div class="text-sm text-gray-900">${item.nama || 'No Name provided'}</div>`;
 
                 // Saran kolom
                 let cell3 = row.insertCell(2);
-                cell3.className = "px-6 py-4 whitespace-nowrap border-b border-gray-200";
+                cell3.className = "px-6 py-4 border-b border-gray-200 min-w-[400px]";
                 cell3.innerHTML = `<div class="text-sm text-gray-900">${item.saran_user || 'No Feedback provided'}</div>`;
 
                 totalLocations++;
