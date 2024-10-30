9915d602fcf2482adb8c3bcd533bd5303b27c79e
FarelNouval <ajahfarel49@gmail.com>
2024-10-30 23:08:16 +0700

    edit

--- javascript/lokasi/lokasi.js
+++ javascript/lokasi/lokasi.js
@@ -44,7 +44,7 @@ function requestLocation() {
                 ],
                 view: new View({
                     center: fromLonLat([longitude, latitude]),
-                    zoom: 15,
+                    zoom: 14,
                 }),
             });
 
@@ -56,7 +56,7 @@ function requestLocation() {
                 new Style({
                     image: new Icon({
                         src: '../img/location-marker.png',
-                        scale: 0.05,
+                        scale: 0.06,
                     }),
                 })
             );
