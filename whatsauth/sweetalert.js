d587c88c884cad9f2243396381157b99ca93aceb
jul003 <dzulkiflifaiz11@gmail.com>
2024-11-01 19:04:57 +0700

    adding sweet alert js in whatsauth

---
+++ whatsauth/sweetalert.js
@@ -0,0 +1,3 @@
+//import js whatsauth yang terbaru
+import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.3.3/whatsauth.js";
+import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.3.3/config.js";
