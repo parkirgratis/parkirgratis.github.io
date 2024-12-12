4296845429b031758cc74be3219480563fe65d50
rayfanaqbil <rayfana09@gmail.com>
2024-12-12 12:09:49 +0700

    changes variable, value, parameter name

--- pages/admin/editwarung.js
+++ pages/admin/editwarung.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    window.showUpdateFormWarung = function (id, namaTempat, lokasi, jamBuka, metodePembayaran, lon, lat, fotoPratinjau) {
+    window.showUpdateFormWarung = function (id, Nama_Tempat, Lokasi, Jam_Buka, Metode_Pembayaran, Lon, Lat, Gambar) {
         console.log("Jam Buka:", jamBuka);
         console.log("fotoPratinjau:", fotoPratinjau);
         document.getElementById('updateIdWarung').value = id;
-        document.getElementById('updateNamaTempatWarung').value = namaTempat || '';
-        document.getElementById('updateLokasiWarung').value = lokasi || '';
-        document.getElementById('updateJamBukaWarung').value = jamBuka || '';
-        document.getElementById('updateMetodePembayaranWarung').value = metodePembayaran || '';
-        document.getElementById('updateLonWarung').value = lon || '';
-        document.getElementById('updateLatWarung').value = lat || '';
-        document.getElementById('updateFotoPratinjauWarung').value = fotoPratinjau || '';
+        document.getElementById('updateNamaTempatWarung').value = Nama_Tempat || '';
+        document.getElementById('updateLokasiWarung').value = Lokasi || '';
+        document.getElementById('updateJamBukaWarung').value = Jam_Buka || '';
+        document.getElementById('updateMetodePembayaranWarung').value = Metode_Pembayaran || '';
+        document.getElementById('updateLonWarung').value = Lon || '';
+        document.getElementById('updateLatWarung').value = Lat || '';
+        document.getElementById('updateFotoPratinjauWarung').value = Gambar || '';
 
         document.getElementById('updateFormContainerWarung').classList.remove('hidden');
     };
@@ -24,15 +24,15 @@ document.addEventListener('DOMContentLoaded', () => {
             e.preventDefault();
 
             const id = document.getElementById('updateIdWarung').value;
-            const namaTempat = document.getElementById('updateNamaTempatWarung').value;
-            const lokasi = document.getElementById('updateLokasiWarung').value;
-            const jamBuka = document.getElementById('updateJamBukaWarung').value;
-            const metodePembayaran = document.getElementById('updateMetodePembayaranWarung').value.split(',');
-            const lon = parseFloat(document.getElementById('updateLonWarung').value);
-            const lat = parseFloat(document.getElementById('updateLatWarung').value);
-            const fotoPratinjau = document.getElementById('updateFotoPratinjauWarung').value;
+            const Nama_Tempat = document.getElementById('updateNamaTempatWarung').value;
+            const Lokasi = document.getElementById('updateLokasiWarung').value;
+            const Jam_Buka = document.getElementById('updateJamBukaWarung').value;
+            const Metode_Pembayaran = document.getElementById('updateMetodePembayaranWarung').value.split(',');
+            const Lon = parseFloat(document.getElementById('updateLonWarung').value);
+            const Lat = parseFloat(document.getElementById('updateLatWarung').value);
+            const Gambar = document.getElementById('updateFotoPratinjauWarung').value;
 
-            if (!id || !namaTempat || !lokasi || !jamBuka || isNaN(lon) || isNaN(lat)) {
+            if (!id || !Nama_Tempat || !Lokasi || !Jam_Buka || isNaN(Lon) || isNaN(Lat)) {
                 Swal.fire('Error', 'All fields must be filled correctly!', 'error');
                 return;
             }
@@ -41,13 +41,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const data = {
                 "_id": id,
-                "nama_tempat": namaTempat,
-                "lokasi": lokasi,
-                "jam_buka": jamBuka,
-                "metode_pembayaran": metodePembayaran,
-                "lon": lon,
-                "lat": lat,
-                "foto_pratinjau": fotoPratinjau,
+                "nama_tempat": Nama_Tempat,
+                "lokasi": Lokasi,
+                "jam_buka": Jam_Buka || '',
+                "metode_pembayaran": Metode_Pembayaran,
+                "lon": Lon,
+                "lat": Lat,
+                "foto_pratinjau": Gambar || ''
             };
 
             try {
