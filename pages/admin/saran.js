261ff4a9118dce37f36de5abd4e36a1353e9c2aa
rayfanaqbil <rayfana09@gmail.com>
2025-02-04 14:07:05 +0700

    changes: endpoint saran

--- pages/admin/saran.js
+++ pages/admin/saran.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const dataDisplayTable = document.getElementById('dataDisplayTable').getElementsByTagName('tbody')[0];
     const totalLocElement = document.getElementById('totalLocations'); 
 
-    fetch('https://asia-southeast2-awangga.cloudfunctions.net/parkirgratis/data/saran')
+    fetch('https://asia-southeast2-awangga.cloudfunctions.net/parkirgratis/data/datasaran')
         .then(response => response.json())
         .then(data => {
             let totalLocations = 0;
